796db9cecff44e33d57c76a254f3f45088926499
davidearthur <davidearthur@gmail.com>
2024-09-09 14:51:06 -0400

    Add missing data warning for load_sw_data

--- R/load_sw_data.R
+++ R/load_sw_data.R
@@ -1,17 +1,20 @@
 #' Load and format data object
 #'
-#' @param period
-#' @param cluster_id
-#' @param individual_id
-#' @param treatment
-#' @param covariates
-#' @param outcome
-#' @param data
+#' @param period A character string; the name of the numeric variable representing the discrete time period of the observation.
+#' @param cluster_id A character string; the name of the numeric variable identifying the cluster.
+#' @param individual_id A character string (optional); the name of the numeric variable identifying the individual.
+#' @param treatment A character string; the name of the binary variable indicating treatment. Accepts either integer (0/1) or Boolean (T/F) values.
+#' @param covariates A character vector; the names of the covariate columns. Columns values should be either numeric, binary, or factors. Character columns will be converted into factors.
+#' @param outcome A character string; the name of the numeric or binary variable indicating treatment. Accepts either numeric or Boolean (T/F) values.
+#' @param data A dataframe containing the stepped wedge trial data.
 #'
-#' @return
+#' @return An object of class \code{sw_data}
 #' @export
 #'
 #' @examples
+#' load_sw_data(period = "period", cluster_id = "id", individual_id = NULL,
+#' treatment = "treatment", covariates = NULL, outcome = "y_bin",
+#' data = geeCRT::sampleSWCRTLarge)
 load_sw_data <- function(
     period, cluster_id, individual_id = NULL, treatment, covariates = NULL, outcome, data
 ) {
@@ -19,7 +22,7 @@ load_sw_data <- function(
   ########## David - work on individual_id and covariates as optional inputs ######
 
   # To prevent R CMD CHECK notes # David question
-  # .time <- .marker <- .covariates <- NULL; rm(.time,.marker,.covariates);
+  .covariates <- .period <- .cluster_id <- .individual_id <- NULL; rm(.covariates, .period, .cluster_id, .individual_id);
 
   # Input validation
   {
@@ -29,8 +32,8 @@ load_sw_data <- function(
     if (methods::is(data,"tbl_df")) { stop("`data` must be a non-tibble data frame.") }
     if (nrow(data)==0) { stop("`data` is an empty data frame.") }
 
-    for (arg in c("period", "cluster_id", "individual_id", "treatment",
-                  "covariates", "outcome")) {
+    for (arg in c("period", "cluster_id", "treatment",
+                  "covariates", "individual_id", "outcome")) {
 
       var <- get(arg)
 
@@ -40,13 +43,24 @@ load_sw_data <- function(
       length_one <- as.logical(length(var) == 1)
       in_df <- all(as.logical(var %in% names(data)))
       if (arg == "covariates") {
-        if (!(is_string && in_df)) {
+        if (!is.null(covariates) && !(is_string && in_df)) {
           stop(
             paste0(
               "`",
               arg,
               "` must be a vector of character strings spe",
               "cifying one or more variables in `data`."
+            )
+          )
+        }
+      } else if (arg == "individual_id") {
+        if (!is.null(individual_id) && !(is_string && length_one && in_df)) {
+          stop(
+            paste0(
+              "`",
+              arg,
+              "` must be a character string specifying a s",
+              "ingle variable in `data`."
             )
           )
         }
@@ -67,33 +81,6 @@ load_sw_data <- function(
       if (arg == "covariates") {
         val <- data[, var, drop = F]
         val2 <- list()
-
-        # David question - Do we want to convert character/factor columns to
-        # indicator columns as in the code below?
-        # # Convert factor/character columns
-        # for (col in names(val)) {
-        #   if (is.numeric(val[, col])) {
-        #     val2[[col]] <- val[, col]
-        #   } else if (is.logical(val[, col])) {
-        #     val2[[col]] <- as.integer(val[, col])
-        #   } else if (is.factor(val[, col]) ||
-        #              is.character(val[, col])) {
-        #     tmp_col <- as.integer(as.factor(val[, col]))
-        #     tmp_unique <- unique(tmp_col)
-        #     tmp_size <- length(tmp_unique)
-        #     for (i in c(1:(tmp_size - 1))) {
-        #       col_new <- paste0(col, "_", i)
-        #       val2[[col_new]] <- as.integer(tmp_col == tmp_unique[i])
-        #     }
-        #   } else {
-        #     stop(paste0("The data type of column `", col,
-        #                 "is ", class(val[, col])
-        #                 # "` is not supported."
-        #                 ))
-        #   }
-        # }
-        # val <- as.data.frame(val2)
-        # x_names <- names(val)
       } else {
         val <- data[, var]
       }
@@ -106,9 +93,21 @@ load_sw_data <- function(
       }
 
       # David question - what types do we want to allow for treatment and outcome?
-      # Currently allowing numeric, 0, 1, F, T for both
-      # Validate: `treatment`, `outcome`
-      if (arg %in% c("treatment", "outcome")) {
+      # Separate treatment (binary) and outcome (and detect whether binary/count/continuous)
+      # Validate: `treatment`
+      if (arg %in% c("treatment")) {
+        if (any(!(val %in% c(0, 1, F, T, NA)))) {
+          stop(paste0(
+            "`",
+            arg,
+            "` must only contain binary values (either T",
+            "/F or 1/0)."
+          ))
+        }
+      }
+
+      # Validate: `outcome`
+      if (arg %in% c("outcome")) {
         if (any(!(val %in% c(0, 1, F, T) | is.numeric(val)))) {
           stop(paste0(
             "`",
@@ -119,31 +118,11 @@ load_sw_data <- function(
         }
       }
 
-      # David question - we want to allow missing values, right?
-      # # No missing values allowed (except marker)
-      # if (!(arg %in% c("marker", "weights", "covariates"))) {
-      #   if (any(is.na(val))) {
-      #     stop("NA values not allowed in `", arg, "`.")
-      #   }
-      # } else if (arg == "covariates") {
-      #   cond_1 <- any(is.na(val))
-      #   cond_2 <- any(is.na(val[as.logical(data[, ph2]), ]))
-      #   msg_1 <- "NA values not allowed in `covariates` (if covariates_ph2==F)."
-      #   msg_2 <- paste0("NA values only allowed in `covariates` for which ph2==F (if covariates_ph2==T).")
-      #   if (cond_1 && !covariates_ph2) {
-      #     stop(msg_1)
-      #   }
-      #   if (cond_2 && covariates_ph2) {
-      #     stop(msg_2)
-      #   }
-      # }
-
       assign(x = paste0(".", arg), value = val)
 
-
     }
 
-    }
+  }
 
 
   # Convert binary variables to integers (if specified as boolean)
@@ -151,13 +130,8 @@ load_sw_data <- function(
   .treatment <- as.integer(.treatment)
 
 
-
-
   .dim_x <- length(.covariates)
 
-  # David question - needed?
-  # .n_v <- sum(.vacc)
-  # .n_p <- sum(1-.vacc)
 
   # David question - do we want to rename covariates?
   # Rename covariate dataframe to c("x1", "x2", ...)
@@ -174,12 +148,22 @@ load_sw_data <- function(
     "treatment" = .treatment
   )
 
+  # Handle missing values
+  # Ignore everything related to covariates for now, only check necessary columns for missingness
+  # Give warning if anything necessary missing (x/y records contain missing data and are being dropped)
+  dat_no_missing <- dat[stats::complete.cases(dat$outcome, dat$period, dat$cluster_id, dat$treatment), ]
 
+  # Count number of rows dropped due to missing values
+  num_dropped <- nrow(dat) - nrow(dat_no_missing)
+  num_total <- nrow(dat)
+
+  message(paste0(num_dropped, "/", num_total, " rows were dropped due to missing values for `cluster_id`, `period`, `treatment`, or `outcome`."))
 
 
   # Create and return data object
 
   # David question - Do we want to assign a class and/or attributes?
+  # maybe add attributes later
   # class(dat) <- c("data.frame", "steppedwedge_dat")
   # attr(dat, "groups") <- .groups
   # attr(dat, "covariate_names") <- x_names
@@ -188,11 +172,20 @@ load_sw_data <- function(
   # attr(dat, "n_vacc") <- .n_v
   # attr(dat, "n_vacc2") <- sum(df_v$z)
   # attr(dat, "n_plac") <- .n_p
+  attr(dat, "n_clusters") <- length(unique(dat$cluster_id))
+  attr(dat, "n_periods") <- length(unique(dat$period))
+  class(dat) <- c("data.frame", "sw_dat")
   return(dat)
 
 }
 
 
-
+#### usethis::use_package(package="sticky", type="Imports", min_version=T)
+#### Unit testing
+#### usethis::use_test("test-core-functions")
+#### usethis::use_vignette(name="basic", title="Basic package workflow")
+#### usethis::use_testthat()
+### Immediate need: design plot, discrete/continuous time, missing data
+### Print method-# of clusters, # of time points, etc.
 
 
