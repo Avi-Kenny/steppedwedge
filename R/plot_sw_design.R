4530e3f449c33fcc7c648193be862f383fbb2b81
Avi Kenny <avi.kenny@gmail.com>
2024-10-06 23:40:01 -0400

    Fixed package name issues

--- R/plot_sw_design.R
+++ R/plot_sw_design.R
@@ -17,32 +17,32 @@ plot_sw_design <- function(data)
   num_periods <- length(unique(data$period))
   # Create a data frame with positions and shading status
   sw_data <- data %>%
-    select(cluster_id, period, treatment) %>%
-    group_by(cluster_id, period) %>%
-    mutate(n = n()) %>%
-    distinct()
+    dplyr::select(cluster_id, period, treatment) %>%
+    dplyr::group_by(cluster_id, period) %>%
+    dplyr::mutate(n = n()) %>%
+    dplyr::distinct()
   summary <- summary(sw_data)
 
   # Plot the grid using ggplot2
-  design_plot <-  ggplot2::ggplot(sw_data, aes(x = period, y = fct_rev(cluster_id),
+  design_plot <-  ggplot2::ggplot(sw_data, ggplot2::aes(x = period, y = forcats::fct_rev(cluster_id),
                                fill = as.character(treatment))) +
-    geom_tile(color = "black") +
-    scale_fill_manual(values = c("1" = "grey", "0" = "white")) +
+    ggplot2::geom_tile(color = "black") +
+    ggplot2::scale_fill_manual(values = c("1" = "grey", "0" = "white")) +
     # reverse order of y axis and label every integer on y axis
-    scale_x_continuous(breaks = seq(min(sw_data$period), max(sw_data$period), by = 1)) +
-    geom_text(aes(label = n), vjust = 1) +
-    theme_minimal() +
-    theme(
-      axis.ticks = element_blank(),
-      panel.grid = element_blank()
+    ggplot2::scale_x_continuous(breaks = seq(min(sw_data$period), max(sw_data$period), by = 1)) +
+    ggplot2::geom_text(aes(label = n), vjust = 1) +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(
+      axis.ticks = ggplot2::element_blank(),
+      panel.grid = ggplot2::element_blank()
     ) +
-    labs(title = "SW design",
+    ggplot2::labs(title = "SW design",
          fill = "Treatment",
          caption = "*Number in each cell represents sample size for the cluster-period.",
          x = "Period",
          y = "Cluster ID") +
     # move legend to bottom
-    theme(legend.position = "bottom")
+    ggplot2::theme(legend.position = "bottom")
 
   return(list(num_clusters = num_clusters,
               num_periods = num_periods,
