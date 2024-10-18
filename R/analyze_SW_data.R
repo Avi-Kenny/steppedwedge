ce5afabdf034f17ebcfd0bc88230ef5e08b10dee
davidearthur <84535945+davidearthur@users.noreply.github.com>
2024-10-18 16:47:56 -0400

    Add analysis methods, change object returned from df to list

--- R/analyze_SW_data.R
+++ R/analyze_SW_data.R
@@ -12,78 +12,126 @@
 #' @examples
 analyze_sw_data <- function(dat, outcome_type, method, estimand, time_varying_assumption) {
   
-  df_results <- data.frame()
+  ### Add input validation
   
-  ##########################################.
-  ##### Immediate Treatment (IT) model #####
-  ##########################################.
+  results <- list()
   
-  # Fit mixed model
-  model_it_mixed <- lme4::lmer(
-    outcome ~ factor(period) + treatment + (1|cluster_id),
-    data = dat
-  )
-  summary(model_it_mixed)
+  if(method == "mixed" & estimand %in% c("TATE", "LTE") & time_varying_assumption == "IT") {
+    
+    ##########################################.
+    ##### Immediate Treatment (IT) model #####
+    ##########################################.
+    
+    # Fit mixed model
+    model_it_mixed <- lme4::lmer(
+      outcome ~ factor(period) + treatment + (1|cluster_id),
+      data = dat
+    )
+    summary(model_it_mixed)
+    
+    # Extract an estimate and confidence interval for the estimated treatment
+    #     effect; recall that the TATE estimator for any interval and the PTE/LTE
+    #     estimators are all equivalent when using the immediate treatment model
+    te_est <- summary(model_it_mixed)$coefficients["treatment",1]
+    te_se <- summary(model_it_mixed)$coefficients["treatment",2]
+    te_ci <- te_est + c(-1.96,1.96) * te_se
+    # te_ci_lower <- te_est + c(-1.96) * te_se
+    # te_ci_upper <- te_est + c(1.96) * te_se
+    
+    results <- list(
+      model = "it_mixed",
+      estimand = "TATE/LTE",
+      te_est = te_est,
+      te_se = te_se,
+      te_ci = te_ci
+      # te_ci_lower = te_ci_lower,
+      # te_ci_upper = te_ci_upper
+    )
+  } else if(method == "mixed" & time_varying_assumption == "ETI") {
   
-  # Extract an estimate and confidence interval for the estimated treatment
-  #     effect; recall that the TATE estimator for any interval and the PTE/LTE
-  #     estimators are all equivalent when using the immediate treatment model
-  te_est <- summary(model_it_mixed)$coefficients["treatment",1]
-  te_se <- summary(model_it_mixed)$coefficients["treatment",2]
-  te_ci_lower <- te_est + c(-1.96) * te_se
-  te_ci_upper <- te_est + c(1.96) * te_se
+    
+    ###############################################.
+    ##### Exposure Time Indicator (ETI) model #####
+    ###############################################.
+    
+    # Fit mixed model
+    model_eti_mixed <- lme4::lmer(
+      outcome ~ factor(period) + factor(exposure_time) + (1|cluster_id),
+      data = dat
+    )
+    summary_eti <- summary(model_eti_mixed)
+    
+    
+    
+    # ############# Remove, just for testing ############# 
+    # 
+    # # Fit mixed model
+    # model_eti_mixed <- lme4::lmer(
+    #   outcome ~ factor(period) + factor(exposure_time) + (1|cluster_id),
+    #   data = test_df
+    # )
+    # summary_eti <- summary(model_eti_mixed)
+    # 
+    # ####################################################
+    
+    
+    ####### Resume here ############
+    
+    # Specify the indices of summary_eti corresponding to the exposure time variables
+    indices <- grep("exposure_time", rownames(summary_eti$coefficients))
+    index_max <- length(indices)
+
+    # Extract coefficient estimates and covariance matrix corresponding to exposure
+    #     time variables
+    coeffs <- summary_eti$coefficients[,1][indices] # column 1 contains the estimates
+    cov_mtx <- vcov(model_eti_mixed)[indices,indices]
+    
+    if(estimand == "TATE") {
+
+      # Estimate the TATE
+      tate_est <- mean(coeffs)
+      # tate_se <- sqrt(mean(cov_mtx)) # David question--line below ok?
+      tate_se <- sqrt(mean(as.matrix(cov_mtx)))
+      tate_ci <- tate_est + c(-1.96,1.96) * tate_se
   
-  new_row <- data.frame(
-    model = "it_mixed",
-    estimand = "TATE/LTE",
-    te_est = te_est,
-    te_se = te_se,
-    te_ci_lower = te_ci_lower,
-    te_ci_upper = te_ci_upper
-  )
+      # # Estimate the TATE (equivalent calculation using
+      # #     matrix multiplication)
+      # M <- matrix(rep(1/index_max), index_max, nrow=1)
+      # tate_est <- (M %*% coeffs)[1]
+      # tate_se <- (sqrt(M %*% cov_mtx %*% t(M)))[1,1]
+      # tate_ci <- tate_est + c(-1.96,1.96) * tate_se
+      
+      results <- list(
+        model = "eti_mixed",
+        estimand = "TATE",
+        te_est = tate_est,
+        te_se = tate_se,
+        te_ci = tate_ci
+      )
+      
+    } else if(estimand == "LTE") {
   
-  ###############################################.
-  ##### Exposure Time Indicator (ETI) model #####
-  ###############################################.
+      # Estimate the LTE
+      lte_est <- as.numeric(coeffs[index_max])
+      lte_se <- sqrt(cov_mtx[index_max,index_max])
+      lte_ci <- lte_est + c(-1.96,1.96) * lte_se
+      
+      results <- list(
+        model = "eti_mixed",
+        estimand = "LTE",
+        te_est = lte_est,
+        te_se = lte_se,
+        te_ci = lte_ci
+      )
+      # 
+      # # Estimate the effect curve
+      # curve_eti <- as.numeric(c(0, coeffs))
+      
+    }
+    
+  }
   
-  # Fit mixed model
-  model_eti_mixed <- lme4::lmer(
-    outcome ~ factor(period) + factor(exposure_time) + (1|cluster_id),
-    data = dat
-  )
-  summary(model_eti_mixed)
-  
-  ####### Resume here ############
-  
-  # # Specify the indices corresponding to the exposure time variables
-  # indices <- c(8:13)
-  # 
-  # # Extract coefficient estimates and covariance matrix corresponding to exposure
-  # #     time variables
-  # coeffs <- summary(model_eti_mixed)$coefficients[,1][indices]
-  # cov_mtx <- vcov(model_eti_mixed)[indices,indices]
-  # 
-  # # Estimate the TATE over the interval [0,6]
-  # tate_est <- mean(coeffs)
-  # tate_se <- sqrt(mean(cov_mtx))
-  # tate_ci <- tate_est + c(-1.96,1.96) * tate_se
-  # 
-  # # Estimate the TATE over the interval [0,6] (equivalent calculation using
-  # #     matrix multiplication)
-  # M <- matrix(rep(1/6, 6), nrow=1)
-  # tate_est <- (M %*% coeffs)[1]
-  # tate_se <- (sqrt(M %*% cov_mtx %*% t(M)))[1,1]
-  # tate_ci <- tate_est + c(-1.96,1.96) * tate_se
-  # 
-  # # Estimate the LTE
-  # lte_est <- as.numeric(coeffs[6])
-  # lte_se <- sqrt(cov_mtx[6,6])
-  # lte_ci <- lte_est + c(-1.96,1.96) * lte_se
-  # 
-  # # Estimate the effect curve
-  # curve_eti <- as.numeric(c(0, coeffs))
-  
-  return(new_row)
+  return(results)
   
   # # Display results
   # display_results("TATE/LTE", te_est, te_ci)
