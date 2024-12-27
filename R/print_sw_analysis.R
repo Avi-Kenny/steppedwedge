9fb0add2b5f9accade2995b13b1cf7adbfdb5963
davidearthur <84535945+davidearthur@users.noreply.github.com>
2024-12-27 09:37:51 -0500

    Add print method for objects from `analysis()`

---
+++ R/print_sw_analysis.R
@@ -0,0 +1,16 @@
+#' Title
+#'
+#' @param x A list returned by steppedwedge::analysis()
+#' @param ... Additional arguments
+#'
+#' @return A summary of the analysis object, including an estimate and 95% CI for the treatment effect, and messages on convergence.
+#' @export
+#'
+#' @examples
+#' # TO DO
+print.sw_analysis <- function(x, ...) {
+  cat("Treatment effect estimate: ", round(x$te_est, 3), "\n", sep = "")
+  cat("Treatment effect 95% confidence interval: ", round(x$te_ci[1], 3), ", ", round(x$te_ci[2], 3), "\n", sep = "")
+  cat("Converged: ", x$converged, "\n", sep = "")
+  cat("Messages returned: ", x$converged2, sep = "")
+}
