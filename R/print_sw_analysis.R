a85b359663709a322fa9874df76155cb28f1c5fc
davidearthur <84535945+davidearthur@users.noreply.github.com>
2025-01-24 14:40:08 -0500

    Added example to `print.sw_analysis`

--- R/print_sw_analysis.R
+++ R/print_sw_analysis.R
@@ -7,7 +7,15 @@
 #' @export
 #'
 #' @examples
-#' # TO DO
+#' # Load data
+#' test_data <- load_data(time ="period", cluster_id = "cluster", individual_id = NULL,
+#' treatment = "trt", outcome = "outcome_bin", data = sw_data_example)
+#' 
+#' # Analysis example: TATE estimand for exposure times 1 through 4
+#' results_tate <- analyze(dat = test_data, method = "mixed", estimand_type = "TATE",
+#' estimand_time = c(1, 4), exp_time = "ETI", family = poisson)
+#' 
+#' results_tate
 print.sw_analysis <- function(x, ...) {
   cat("Treatment effect estimate: ", round(x$te_est, 3), "\n", sep = "")
   cat("Treatment effect 95% confidence interval: ", round(x$te_ci[1], 3), ", ", round(x$te_ci[2], 3), "\n", sep = "")
