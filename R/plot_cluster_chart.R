4a4ed6111fc2f74d5fced59395459fefd0d05c4c
davidearthur <84535945+davidearthur@users.noreply.github.com>
2025-10-19 19:34:43 -0400

    Add `plot_cluster_chart()`

---
+++ R/plot_cluster_chart.R
@@ -0,0 +1,49 @@
+#' Plot cluster chart
+#'
+#' @param analysis_object A list of class `sw_analysis`.
+#'
+#' @returns A list with a plot of the actual and predicted outcomes by cluster.
+#' @export
+#'
+#' @examples
+#' # Load data
+#' test_data <- load_data(time ="period", cluster_id = "cluster", individual_id = NULL,
+#' treatment = "trt", outcome = "outcome_cont", data = sw_data_example)
+#'
+#' # Analyze using TATE estimand for exposure times 1 through 4
+#' results_tate <- analyze(dat = test_data, method = "mixed", estimand_type = "TATE",
+#' estimand_time = c(1, 4), exp_time = "ETI")
+#' 
+#' # Plot by cluster
+#' plot_cluster_chart(results_tate)
+#' 
+plot_cluster_chart <- function(analysis_object)
+{
+  # Prevent R CMD CHECK note
+  # aes <- cluster_id <- first_exposure <- n <- time <- treatment <- x <- NULL
+  # rm(aes,cluster_id,first_exposure,n,time,treatment,x)
+  
+  # Input validation
+  if (!methods::is(analysis_object,"sw_analysis")) { stop("`analysis_object` must be of class `sw_analysis`.") }
+  
+  dat <- analysis_object$dat
+  
+  dat$preds <- as.numeric(predict(analysis_object$model))
+
+  dat$cluster <- factor(paste("Cluster", dat$cluster_id))
+
+  cluster_order <- order(as.numeric(unique(gsub("Cluster ", "", dat$cluster))))
+  dat$cluster <- factor(dat$cluster, levels = paste("Cluster", cluster_order))
+  
+  cluster_chart <- ggplot(dat, aes(x=time, y=outcome, color=factor(treatment))) +
+    geom_point(alpha=0.5) +
+    geom_line(aes(y=preds), color="darkred", linewidth=1) +
+    labs(color="Treatment", x="Time", y="Outcome") +
+    scale_color_manual(values=c("#E69F00", "#009E73")) +
+    facet_wrap(~cluster, ncol=4) +
+    # move legend to bottom
+    ggplot2::theme(legend.position = "bottom")
+  
+  return(list(cluster_chart = cluster_chart))
+
+}
