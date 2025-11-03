429a7bc8927d020f90b6203bed7a254b1487d39b
davidearthur <84535945+davidearthur@users.noreply.github.com>
2025-11-03 13:16:55 -0500

    Make treatment effect in example dataset non-null

--- data-raw/sw_data_example.R
+++ data-raw/sw_data_example.R
@@ -1,11 +1,14 @@
 set.seed(2278)
-expit <- function(x) { 1 / (1+exp(-x))}
 
 sw_data_example <- data.frame(cluster = 1:15) %>%
   # expand data_sw with period 1 through 6 for each cluster
   tidyr::expand(cluster, period = 1:6) %>%
   # generate cluster period sizes
   dplyr::mutate(cp_size = sample(15:30, dplyr::n(), replace = TRUE))
+
+# treatment effects
+b_bin <- log(2)
+b_cont <- 0.5
 
 sw_data_example %<>%
   # create individual observations for each cluster period
@@ -23,15 +26,17 @@ sw_data_example %<>%
   dplyr::ungroup() %>%
   # generate binary outcomes
   dplyr::rowwise() %>%
-  dplyr::mutate(prob = expit(0.2 + cluster_re + cluster_time_re)) %>%
+  dplyr::mutate(prob = plogis(0.2 + b_bin * trt + cluster_re + cluster_time_re)) %>%
   dplyr::ungroup() %>%
   dplyr::mutate(outcome_bin = rbinom(n = dplyr::n(), size = 1, prob = prob)) %>%
   # generate continuous outcomes
-  dplyr::mutate(outcome_cont = rnorm(n = dplyr::n(), mean = (cluster_re+cluster_time_re), sd = 1)) %>%
+  dplyr::mutate(outcome_cont = rnorm(n = dplyr::n(), 
+                                     mean = (b_cont * trt + cluster_re+cluster_time_re),
+                                     sd = 1)) %>%
   # generate binomial outcomes
   mutate(denominator = floor(runif(nrow(.), min = 5, max = 15))) %>%
   rowwise() %>%
-  mutate(numerator = rbinom(1, denominator, 0.5)) %>%
+  mutate(numerator = rbinom(1, denominator, prob)) %>%
   ungroup() %>%
   dplyr::select(-c(seq, dplyr::ends_with("_re"))) %>%
   # convert to non-tibble dataframe
