462d4fbb41d54d5ff4a177a2a124494a6e8aa509
davidearthur <84535945+davidearthur@users.noreply.github.com>
2025-12-11 10:48:09 -0500

    Move example binomial outcome data to separate dataset

--- R/data.R
+++ R/data.R
@@ -3,7 +3,7 @@
 #' Data generated for the purpose of demonstrating the steppedwedge package
 #'
 #' @format ## `sw_data_example`
-#' A data frame with 2,063 rows and 6 columns:
+#' A data frame with 2,063 rows and 5 columns:
 #' \describe{
 #'   \item{cluster}{Cluster id}
 #'   \item{period}{Time period}
@@ -13,3 +13,20 @@
 #'   ...
 #' }
 "sw_data_example"
+
+#' Example stepped wedge data with aggregrated outcomes
+#'
+#' A simulated stepped wedge dataset formatted with separate columns for
+#' the number of successes (numerator) and number of trials (denominator).
+#'
+#' @format ## `sw_data_example_binom`
+#' A data frame with 90 rows and 5 columns:
+#' \describe{
+#'   \item{cluster}{Cluster id}
+#'   \item{period}{Time period}
+#'   \item{trt}{Treatment indicator}
+#'   \item{denominator}{Denominator (# trials)}
+#'   \item{numerator}{Numerator (# successes)}
+#'   ...
+#' }
+"sw_data_example_binom"
--- data-raw/sw_data_example_binom.R
+++ data-raw/sw_data_example_binom.R
@@ -1,6 +1,6 @@
 set.seed(2278)
 
-sw_data_example <- data.frame(cluster = 1:15) %>%
+sw_data_example_binom <- data.frame(cluster = 1:15) %>%
   # expand data_sw with period 1 through 6 for each cluster
   tidyr::expand(cluster, period = 1:6) %>%
   # generate cluster period sizes
@@ -10,9 +10,7 @@ sw_data_example <- data.frame(cluster = 1:15) %>%
 b_bin <- log(2)
 b_cont <- 0.5
 
-sw_data_example %<>%
-  # create individual observations for each cluster period
-  tidyr::uncount(weights = cp_size) %>%
+sw_data_example_binom %<>%
   # assign sequences
   dplyr::mutate(seq = ceiling(cluster / 3)) %>%
   # assign treatment indicator
@@ -24,17 +22,16 @@ sw_data_example %<>%
   dplyr::group_by(cluster, period) %>%
   dplyr::mutate(cluster_time_re = rnorm(n = 1, mean = 0, sd = 0.1)) %>%
   dplyr::ungroup() %>%
-  # generate binary outcomes
-  dplyr::rowwise() %>%
+  # generate probability for each cluster period
   dplyr::mutate(prob = plogis(0.2 + b_bin * trt + cluster_re + cluster_time_re)) %>%
+  # generate binomial outcomes
+  dplyr::mutate(denominator = cp_size) %>%
+  dplyr::rowwise() %>%
+  dplyr::mutate(numerator = rbinom(1, denominator, prob)) %>%
   dplyr::ungroup() %>%
-  dplyr::mutate(outcome_bin = rbinom(n = dplyr::n(), size = 1, prob = prob)) %>%
-  # generate continuous outcomes
-  dplyr::mutate(outcome_cont = rnorm(n = dplyr::n(), 
-                                     mean = (b_cont * trt + cluster_re+cluster_time_re),
-                                     sd = 1)) %>%
-  dplyr::select(-c(seq, prob, dplyr::ends_with("_re"))) %>%
+  dplyr::select(-c(seq, cp_size, prob, dplyr::ends_with("_re"))) %>%
   # convert to non-tibble dataframe
   data.frame()
 
-usethis::use_data(sw_data_example, overwrite = TRUE)
+
+usethis::use_data(sw_data_example_binom, overwrite = TRUE)
